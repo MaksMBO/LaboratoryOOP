41797b502458905b5c449717ad26d447c77fbb41
MaksMBO <Maksbredyk@gmail.com>
2021-11-11 14:12:34 +0200

    debug

--- practice21_11_11/1.py
+++ practice21_11_11/1.py
@@ -1,14 +1,13 @@
 import os
 import timeit
 
-# with open('asd.txt', 'w') as open_file:
-#     while os.path.getsize('asd.txt') / 1000000 < 50:
-#         open_file.write(
-#             '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890\n')
+with open('asd.txt', 'w') as open_file:
+    while os.path.getsize('asd.txt') / 1000000 < 50:
+        open_file.write(
+            '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890\n')
 
 
-
-s="""
+s = """
 with open("asd.txt") as file:
     sum = 0
     lines = file.readlines()
@@ -27,12 +26,11 @@ with open("asd.txt") as file:
 """
 print(timeit.timeit(s, number=1))
 
-s ="""
+s = """
 with open("asd.txt") as file:
     s = 0
     k = (int(symbol.strip()) for symbol in file if symbol.strip().isdigit())
     s = sum(k)
 """
-
 
 print(timeit.timeit(s, number=1))
