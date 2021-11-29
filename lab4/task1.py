ff3438707855cbff1606fc6abcf1e22a1710cbc3
MaksMBO <Maksbredyk@gmail.com>
2021-11-29 23:40:46 +0200

    rewritten functions with arithmetic operations, added the ability to compare two fractions

--- lab4/task1.py
+++ lab4/task1.py
@@ -19,34 +19,50 @@ class Rational:
     def result(self):
         return self.__numerator / self.__denominator
 
-    def multiplication(self, new_numerator=1, new_denominator=1):
-        if not isinstance(new_numerator, int) or not isinstance(new_denominator, int):
+    def __mul__(self, other):
+        if not isinstance(other, Rational):
             raise TypeError("The entered value is of the wrong type")
-        self.__numerator = self.__numerator * new_numerator
-        self.__denominator = self.__denominator * new_denominator
+        self.__numerator = self.__numerator * other.__numerator
+        self.__denominator = self.__denominator * other.__denominator
 
-    def division(self, new_numerator=1, new_denominator=1):
-        if not isinstance(new_numerator, int) or not isinstance(new_denominator, int):
+    def __truediv__(self, other):
+        if not isinstance(other, Rational):
             raise TypeError("The entered value is of the wrong type")
-        self.__numerator = self.__numerator * new_denominator
-        self.__denominator = self.__denominator * new_numerator
+        if other.__denominator == 0:
+            raise ZeroDivisionError("Cannot be divided by zero!")
+        self.__numerator = self.__numerator * other.__denominator
+        self.__denominator = self.__denominator * other.__numerator
 
-    def sum(self, new_numerator=0, new_denominator=1):
-        if not isinstance(new_numerator, int) or not isinstance(new_denominator, int):
+    def __add__(self, other):
+        if not isinstance(other, Rational):
             raise TypeError("The entered value is of the wrong type")
-        self.__numerator = self.__numerator * new_denominator + self.__denominator * new_numerator
-        self.__denominator = self.__denominator * new_denominator
+        self.__numerator = self.__numerator * other.__denominator + self.__denominator * other.__numerator
+        self.__denominator = self.__denominator * other.__denominator
 
-    def minus(self, new_numerator=0, new_denominator=1):
-        if not isinstance(new_numerator, int) or not isinstance(new_denominator, int):
+    def __sub__(self, other):
+        if not isinstance(other, Rational):
             raise TypeError("The entered value is of the wrong type")
-        self.__numerator = self.__numerator * new_denominator - self.__denominator * new_numerator
-        self.__denominator = self.__denominator * new_denominator
+        self.__numerator = self.__numerator * other.__denominator - self.__denominator * other.__numerator
+        self.__denominator = self.__denominator * other.__denominator
+
+    def __eq__(self, other):
+        if not isinstance(other, Rational):
+            raise TypeError("The entered value is of the wrong type")
+        if self.__numerator == other.__numerator:
+            if self.__denominator == other.__denominator:
+                return "They are equal"
+        return "They are not equal"
+
+    def __str__(self):
+        return "/".join(map(str, rational.fraction_reduction()))
 
 
 rational = Rational(2, 6)
-rational.division(4, 6)
-result = "/".join(map(str, rational.fraction_reduction()))
-print(f"Simple fraction: {result}")
-print(f"In decimal: {rational.result()}")
+rational2 = Rational(-1, 6)
+rational3 = Rational(2, 6)
 
+print(rational == rational2)
+print(rational == rational3)
+
+print(f"Simple fraction: {rational}")
+print(f"In decimal: {rational.result()}")
