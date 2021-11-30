20f27a60c0dba4ba6ceaa1219f390636f05c9629
MaksMBO <Maksbredyk@gmail.com>
2021-11-30 15:48:03 +0200

    created a calendar class, added getters and setters, method __str__

---
+++ lab4/task1(2 TI-01).py
@@ -0,0 +1,38 @@
+class Calendar:
+    def __init__(self, day, month, year):
+        self.__day = day
+        self.__month = month
+        self.__year = year
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, day):
+        if not isinstance(day, int):
+            raise TypeError("The value must be a int")
+        self.__day = day
+
+    @property
+    def month(self):
+        return self.__month
+
+    @month.setter
+    def month(self, month):
+        if not isinstance(month, int):
+            raise TypeError("The value must be a int")
+        self.__month = month
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, year):
+        if not isinstance(year, int):
+            raise TypeError("The value must be a int")
+        self.__year = year
+
+    def __str__(self):
+        return f""
